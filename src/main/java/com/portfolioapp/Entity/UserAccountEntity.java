93138b270acf2c0d00a0090125ffcb859306d7f6
Sam Kling <samtkling@gmail.com>
2022-11-19 21:55:24 -0500

    use data annotation instead of getters and setters

--- src/main/java/com/portfolioapp/Entity/UserAccountEntity.java
+++ src/main/java/com/portfolioapp/Entity/UserAccountEntity.java
@@ -1,10 +1,13 @@
 package com.portfolioapp.Entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
+@Data
 @Table(name = "user_account", schema = "public", catalog = "postgres")
 public class UserAccountEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,54 +29,6 @@ public class UserAccountEntity {
     @Basic
     @Column(name = "last_login")
     private Timestamp lastLogin;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Timestamp getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(Timestamp createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public Timestamp getLastLogin() {
-        return lastLogin;
-    }
-
-    public void setLastLogin(Timestamp lastLogin) {
-        this.lastLogin = lastLogin;
-    }
 
     @Override
     public boolean equals(Object o) {
